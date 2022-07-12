30038c3d4e7315ad3040ef1fd6577d2817797951
Anna Akhmetgaleeva <78921345+anna-akhmet@users.noreply.github.com>
2022-07-12 13:55:04 +0300

    Add mapbox worker class and directives
    To prevent npm build fron transpiling mapbox

--- src/useMapbox.js
+++ src/useMapbox.js
@@ -1,6 +1,11 @@
 import React, {useLayoutEffect } from "react";
 import mapboxgl from "mapbox-gl/dist/mapbox-gl.js";
 
+// / The following is required to stop "npm build" from transpiling mapbox code.
+// @ts-ignore
+// eslint-disable-next-line import/no-webpack-loader-syntax, import/no-unresolved
+mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
+
 export default function useMapbox(container, center, zoom) {
     mapboxgl.accessToken = "pk.eyJ1IjoiYW5uYWFraG1ldCIsImEiOiJjbDJoZjdybTAwZDllM2lwaGpwMjZreHhsIn0.VpRx5KdVw-9fAvWYptlseg";
 
